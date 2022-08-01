1b1e6224f6420cf487f522596f45c43c47654068
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2022-08-01 08:17:29 +0800

    test2

---
+++ precache-manifest.892d4023da94e8dda5c94635ff802929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde750b256b366e6bcfefb6023dbb16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964dbf04af3bb96bcc86",
+    "url": "/static/css/2.a1c91b15.chunk.css"
+  },
+  {
+    "revision": "964dbf04af3bb96bcc86",
+    "url": "/static/js/2.db048cf5.chunk.js"
+  },
+  {
+    "revision": "04f8113689b490503efd2991dc5cbb53",
+    "url": "/static/js/2.db048cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257bbf789637896ec3ee",
+    "url": "/static/js/main.51ca057b.chunk.js"
+  },
+  {
+    "revision": "97a3ee8976b2ed78bf60",
+    "url": "/static/js/runtime-main.fd90b156.js"
+  }
+]);
